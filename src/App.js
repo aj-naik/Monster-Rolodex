0cce3d3d2aac23b63fade444a8787f3d8526660a
Ajinkya Naik <51918054+aj-naik@users.noreply.github.com>
2021-04-30 18:16:53 +0530

    Monster Rolodex Website. First React project commit

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import './App.css';
+import React, { Component } from 'react';
+
+import { CardList } from './components/card-list/card-list.component';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [
+        {
+          name: 'Maitri',
+          id: 'rutvik'
+        },
+        {
+          name: 'Manan',
+          id: 'maitri'
+        },
+        {
+          name: 'Krisha',
+          id: 'harsh'
+        }
+      ]
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <CardList monsters = {this.state.monsters}>  
+        </CardList>
+      </div>
+    );
+  }
+}
+export default App;
